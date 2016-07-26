98498da0037f1a31484c1bcc60955c96b7a0d92b
AlpixTM <contact@alpixmc.de>
2016-07-26 22:29:33 +0200

    New loader

---
+++ apply/loader.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'dbconf.php';
+
+//variables
+$dev=TRUE;
+$ip=$_SERVER['REMOTE_ADDR'];
+$date = date("d-m-y");
+$site=$_GET["site_app"];
+
+
+// dynamic variables & settings
+if ($dev==true) {
+	echo $site;
+}
+if ($site=="") {
+	$site="main";
+}
+if ($dev==TRUE) {
+	error_reporting(E_ALL);
+}
+$link = mysqli_connect ( MYSQL_HOST, MYSQL_BENUTZER, MYSQL_KENNWORT, MYSQL_DATENBANK, MYSQL_PORT );
+if (!$link) {
+	die('Connect Error: ' . mysqli_connect_error());
+}
+
+
+?>
