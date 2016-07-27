6a0fd723832ba21ec812f55aeb2fa2220379612e
AlpixTM <contact@alpixmc.de>
2016-07-28 00:53:36 +0200

    db-config values

--- apply/loader.php
+++ apply/loader.php
@@ -6,8 +6,7 @@ require_once 'fallback-conf.php';
 //variables
 $ip=$_SERVER['REMOTE_ADDR'];
 $date = date("d-m-y");
-
-
+require_once 'db-config-read.php';
 
 // dynamic variables & settings
 $site=$_GET["$urlparam"];
