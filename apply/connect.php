f5e4959a5390b56228f529470826f5073c44dbc6
AlpixTM <contact@alpixmc.de>
2016-08-15 15:20:20 +0200

    Only showing error infos if $dev=true!

--- apply/connect.php
+++ apply/connect.php
@@ -1,7 +1,12 @@
 <?php
 $link = mysqli_connect ( MYSQL_HOST, MYSQL_BENUTZER, MYSQL_KENNWORT, MYSQL_DATENBANK, MYSQL_PORT );
 if (!$link) {
-	die('Connect Error: ' . mysqli_connect_error());
+    if ($dev=="true") {
+        die('Connect Error: ' . mysqli_connect_error());
+    }
+    else{
+        die('Connect Error! (Enable Dev for more informations.)');
+    }
 }
 mysqli_set_charset ( $link, 'utf8' );
 ?>
