410e2fdd17f64ba2f6ed2a1016dc235cd0d8d673
Alpix <Lukasniekum@googlemail.com>
2017-01-15 13:10:26 +0100

    Update dbconf.php

--- dbconf.php
+++ dbconf.php
@@ -1,5 +1,5 @@
 <?php
-define ( 'MYSQL_KENNWORT',  'bo8o7mvo8XiVHHKi' );
+define ( 'MYSQL_KENNWORT',  'ZENSIERT' );
 define ( 'MYSQL_HOST',      'localhost' );
 define ( 'MYSQL_BENUTZER',  'public_apply' );
 define ( 'MYSQL_DATENBANK', 'public_apply' );
